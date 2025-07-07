2f10b41dfbadea0cccb6b0f6f28b8b46fa9279d5
Alejandro Calle <>
2025-07-07 19:46:29 +0200

    feature(BottomBar_Refactor): reformatted to fix indentation

--- Assets/0_Project/Modules/HomeScreen/Footer/Scripts/BottomBarView.cs
+++ Assets/0_Project/Modules/HomeScreen/Footer/Scripts/BottomBarView.cs
@@ -3,112 +3,116 @@ using DG.Tweening;
 using UnityEngine;
 
 
-    public class BottomBarView : MonoBehaviour
+public class BottomBarView : MonoBehaviour
+{
+    [Header("Components")] [SerializeField]
+    private GameObject _selectionIndicator;
+
+    [SerializeField] private BottomBarButton _defaultSelection;
+    [SerializeField] private List<BottomBarButton> _bottomBarButtons;
+
+    [Header("Transition Parameters")] [SerializeField]
+    private float _transitionDuration = 0.25f;
+
+    [SerializeField] private Ease _transitionEaseType = Ease.OutSine;
+
+    //Internal
+    private BottomBarButton _buttonSelected;
+    private BottomBarButton _currentSelection;
+
+    void Start()
     {
-        [Header("Components")]
-        [SerializeField] private GameObject _selectionIndicator;
-        [SerializeField] private BottomBarButton _defaultSelection;
-        [SerializeField] private List<BottomBarButton> _bottomBarButtons;
-        
-        [Header("Transition Parameters")]
-        [SerializeField] private float _transitionDuration = 0.25f;
-        [SerializeField] private Ease _transitionEaseType = Ease.OutSine;
+        Initialize();
+    }
 
-        //Internal
-        private BottomBarButton _buttonSelected;
-        private BottomBarButton _currentSelection;
-
-        void Start()
+    private void Initialize()
+    {
+        if (_defaultSelection != null)
         {
-            Initialize();
+            OnButtonClickedEvent(_defaultSelection);
         }
-
-        private void Initialize()
+        else
         {
-            if (_defaultSelection != null)
-            {
-                OnButtonClickedEvent(_defaultSelection);
-            }
-            else
-            {
-                _selectionIndicator.SetActive(false);
-            }
-        }
-        
-        private void OnEnable()
-        {
-            foreach (var btn in _bottomBarButtons)
-            {
-                btn.OnButtonClickedEvent.AddListener(OnButtonClickedEvent);
-            }
-        }
-
-        private void OnDisable()
-        {
-            foreach (var btn in _bottomBarButtons)
-            {
-                btn.OnButtonClickedEvent.RemoveListener(OnButtonClickedEvent);
-            }
-        }
-
-        private void OnButtonClickedEvent(BottomBarButton clickedButton)
-        {
-            if (!_bottomBarButtons.Contains(clickedButton)) return;
-           
-            var wasAlreadySelected = _buttonSelected == clickedButton;
-
-            if (wasAlreadySelected)
-            {
-                DeselectAllButtons();
-                return;
-            }
-            SelectButton(clickedButton);
-        }
-        
-        private void DeselectAllButtons()
-        {
-            _buttonSelected = null;
-            _currentSelection = null;
-
-            foreach (var btn in _bottomBarButtons)
-            {
-                btn.SetSelect(false);
-            }
-
             _selectionIndicator.SetActive(false);
         }
-        
-        private void SelectButton(BottomBarButton button)
+    }
+
+    private void OnEnable()
+    {
+        foreach (var btn in _bottomBarButtons)
         {
-            _buttonSelected = button;
-
-            foreach (var btn in _bottomBarButtons)
-            {
-                btn.SetSelect(btn == _buttonSelected);
-            }
-
-            AnimateIndicatorToSelected();
-        }
-
-        private void AnimateIndicatorToSelected()
-        {
-            if (_buttonSelected == null || _currentSelection == _buttonSelected)
-            {
-                return;
-            }
-
-            _currentSelection = _buttonSelected;
-            
-            var targetX = _currentSelection.transform.position.x;
-            
-            _selectionIndicator.SetActive(true);
-            _selectionIndicator.transform.DOKill();
-            _selectionIndicator.transform.DOMoveX(targetX, _transitionDuration)
-                .SetEase(_transitionEaseType).OnComplete(() =>
-            {
-                _selectionIndicator.transform.position = new Vector3(targetX,
-                                                            _selectionIndicator.transform.position.y,
-                                                            _selectionIndicator.transform.position.z);
-            });
+            btn.OnButtonClickedEvent.AddListener(OnButtonClickedEvent);
         }
     }
+
+    private void OnDisable()
+    {
+        foreach (var btn in _bottomBarButtons)
+        {
+            btn.OnButtonClickedEvent.RemoveListener(OnButtonClickedEvent);
+        }
+    }
+
+    private void OnButtonClickedEvent(BottomBarButton clickedButton)
+    {
+        if (!_bottomBarButtons.Contains(clickedButton)) return;
+
+        var wasAlreadySelected = _buttonSelected == clickedButton;
+
+        if (wasAlreadySelected)
+        {
+            DeselectAllButtons();
+            return;
+        }
+
+        SelectButton(clickedButton);
+    }
+
+    private void DeselectAllButtons()
+    {
+        _buttonSelected = null;
+        _currentSelection = null;
+
+        foreach (var btn in _bottomBarButtons)
+        {
+            btn.SetSelect(false);
+        }
+
+        _selectionIndicator.SetActive(false);
+    }
+
+    private void SelectButton(BottomBarButton button)
+    {
+        _buttonSelected = button;
+
+        foreach (var btn in _bottomBarButtons)
+        {
+            btn.SetSelect(btn == _buttonSelected);
+        }
+
+        AnimateIndicatorToSelected();
+    }
+
+    private void AnimateIndicatorToSelected()
+    {
+        if (_buttonSelected == null || _currentSelection == _buttonSelected)
+        {
+            return;
+        }
+
+        _currentSelection = _buttonSelected;
+
+        var targetX = _currentSelection.transform.position.x;
+
+        _selectionIndicator.SetActive(true);
+        _selectionIndicator.transform.DOKill();
+        _selectionIndicator.transform.DOMoveX(targetX, _transitionDuration)
+            .SetEase(_transitionEaseType)
+            .OnComplete(() =>
+            {
+                _selectionIndicator.transform.position = new Vector3(targetX,
+                    _selectionIndicator.transform.position.y,
+                    _selectionIndicator.transform.position.z);
+            });
+    }
+}
